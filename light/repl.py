bfe008c0b1e9c68bd7469248a1c172f0d9335032
Abhai Kollara <abhai.dilip@gmail.com>
2020-04-02 22:31:21 +0530

    Rudimentary REPL

---
+++ light/repl.py
@@ -0,0 +1,35 @@
+from parser import parse_source
+from evaluator import Evaluator, Environment
+
+py_eval = eval
+
+
+class Repl:
+    def __init__(self):
+        self.parse = parse_source
+        self.evaluator = Evaluator()
+        self.env = Environment()
+
+    def eval(self, ast):
+        return self.evaluator.eval(ast[0], self.env)
+
+    def start(self):
+        while True:
+            print(">>>", end=" ")
+            try:
+                line = str(input())
+                ast = self.parse(line)
+
+                val = self.eval(ast)
+                if val is not None:
+                    print(val)
+            except KeyboardInterrupt:
+                print("\nThank You")
+                exit()
+            except Exception as e:
+                print(e)
+
+
+if __name__ == '__main__':
+    r = Repl()
+    r.start()
